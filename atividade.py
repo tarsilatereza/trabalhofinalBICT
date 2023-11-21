cd8f44dcc1f26906839616d1e8cb1b1881f35783
tarsilatereza <tarsilaterezaprm@gmail.com>
2023-11-21 08:04:36 -0300

    resposta incompleta da primeira questão da lista

---
+++ atividade.py
@@ -0,0 +1,12 @@
+# Contar as ocorrências de um elemento em um vetor de inteiros
+
+vetor = []
+i = 0
+cont = 0
+def adicionando():
+	elementos = int(input("Digite os valores do vetor: "))
+	vetor.append(elementos)
+
+while True:
+	for x in range(10):
+		pass
